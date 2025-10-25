78e29294046ff1a392b71735989634e58c82393a
karamjit <karamjit@macbook.local>
2025-10-25 19:58:40 -0400

    portoflio

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Col, Row } from "react-bootstrap";
 import macOs from "../../Assets/TechIcons/Apple MacOSX.svg";
 import windows from "../../Assets/TechIcons/windows.svg";
+import redhat from "../../Assets/TechIcons/redhat.svg";
+import ubuntu from "../../Assets/TechIcons/ubuntu.svg";
 import { 
 
   SiRedhat,
@@ -16,14 +18,12 @@ function Toolstack() {
         <img src={macOs} alt="macOs" className="tech-icon-images" />
         <div className="tech-icons-text">Mac Os</div>
       </Col>
-            <Col xs={4} md={2} className="tech-icons">
-        <SiRedhat fontSize={"24px"} />
+      <Col xs={4} md={2} className="tech-icons">
+        <img src={redhat} alt="RHEL" className="tech-icon-images" />
         <div className="tech-icons-text">RHEL</div>
       </Col>
-
-
       <Col xs={4} md={2} className="tech-icons">
-        <SiUbuntu fontSize={"24px"} />
+        <img src={ubuntu} alt="RHEL" className="tech-icon-images" />
         <div className="tech-icons-text">Ubuntu</div>
       </Col>
 
